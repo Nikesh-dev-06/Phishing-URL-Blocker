27991467027091f5fcf9ad03465956f16593b2cd
Nikesh Babu S <nikes.dev.06@gmail.com>
2026-01-31 21:30:28 +0530

    Update warning.js

--- extension/warning.js
+++ extension/warning.js
@@ -1,9 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 const reason = params.get("reason");
-
 document.getElementById("reason").innerText =
   reason || "This website is unsafe";
-
 document.getElementById("goBack").addEventListener("click", () => {
   history.back();
 });
